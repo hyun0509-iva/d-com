e5ff00e3955284cae1531216e31491bee16454c0
hyun0509-iva <skyblue5030@gmail.com>
2024-03-29 12:03:54 +0900

    message 페이지 구현

--- src/app/(afterLogin)/messages/Page.tsx
+++ src/app/(afterLogin)/messages/Page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import style from "./message.module.css";
+import Room from "./_components/Room";
 
 const Page = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </>
+  );
+};
 
-export default Page
+export default Page;
